03dec42e4206684782736cca7cc7d695e6a7b355
justkhant <khant.kky@gmail.com>
2021-03-28 03:47:17 -0400

    Add eslint and prettier configs for code style/formatter and add temp title page.

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+const Title = () => {
+  return (
+    <div id="title-container">
+      <p id="title">#theprojectmyanmar</p>
+    </div>
+  );
+};
+
+export default Title;
